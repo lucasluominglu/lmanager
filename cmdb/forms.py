00e597d1f2b87b307b63510c6b32c722dec179ae
Lucas Luo 骆明禄 (Systex <lucas.luo@aswgcn.asiapacific.aswgroup.net>
2017-11-16 18:07:47 +0800

    增加新添主机，编辑主机，差编辑详细信息，IP：hostlist显示故障

--- cmdb/forms.py
+++ cmdb/forms.py
@@ -1,9 +1,24 @@
 from django import forms
-from cmdb.models import Idc
+from cmdb.models import *
 
 
 class IdcForm(forms.ModelForm):
     class Meta:
         model = Idc
         fields = ['name', 'remark']
-        labels = {'name': '', 'remark': ''}
+        labels = {'name': '机房', 'remark': '备注'}
+
+
+class HostForm(forms.ModelForm):
+	class Meta:
+		model = Host
+		fields = ['idc','hostname','num','application']
+		labels = {'idc': '机房', 'hostname': '主机名','num': '编号', 'application': '应用'}
+
+
+class HostInfoFrom(forms.ModelForm):
+	class Meta:
+		model = HostInfo
+		fields = ['host','manufacturer','productmode','serialnumber','cpu','mem','os','disk']
+		labels = {'host':'主机', 'manufacturer':'厂商','productmode':'产品型号',
+		 'serialnumber':'产品序列号', 'cpu':'CPU核数', 'mem':'内存', 'os':'操作系统', 'disk':'硬盘大小' }
--- cmdb/views.py
+++ cmdb/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, Http404
 from django.core.urlresolvers import reverse
 from cmdb.models import *
-from .forms import IdcForm
+from .forms import *
 
 
 def index(request):
@@ -15,6 +15,24 @@ def idc(request):
     idc = Idc.objects.all()
     context = {'idc': idc}
     return render(request, 'cmdb/idc.html', context)
+
+def hostlist(request,host_id):
+    hostlist = Host.objects.all()
+    #hostips = Host.objects.first().ip_set.all()
+    hosts = Host.objects.get(id=host_id)
+    hostips = hosts.ip_set.all()
+    context = {'hostlist':hostlist,'hosts': hosts,'hostips':hostips}
+    return render(request, 'cmdb/hostlist.html', context)
+
+def hostinfo(request, host_id):
+    #hosts = Host.objects.all()
+    #hostips = Host.objects.first().ip_set.all()
+    hosts = Host.objects.get(id=host_id)
+    hostinfos = hosts.hostinfo_set.all()
+    hostips = hosts.ip_set.all()
+    context = {'hosts': hosts,'hostips': hostips,'hostinfos':hostinfos}
+    return render(request, 'cmdb/hostinfo.html', context)
+
 
 
 def new_idc(request):
@@ -37,30 +55,52 @@ def edit_idc(request, idc_id):
     if request.method != 'POST':
         form = IdcForm(instance=idc)
     else:
-        form = IdcForm(instance=entry, data=request.POST)
+        form = IdcForm(instance=idc, data=request.POST)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect(
-                reverse('learning_logs:topic', args=[topic.id]))
+                reverse('cmdb:idc', args=[idc.id]))
     context = {'idc': idc, 'name': name, 'remark': remark, 'form': form}
     return render(request, 'cmdb/edit_idc.html', context)
 
+def edit_host(request, host_id):
+    host = Host.objects.get(id=host_id)
+    hostname = host.hostname
+    num = host.num
+    application = host.application
 
-def hostlist(request):
-    hlist = Host.objects.all()
-    #hostips = Host.objects.first().ip_set.all()
-    #hosts = Host.objects.get(id=host_id)
-   # hostips = hosts.ip_set.all()
-    context = {'hlist':hlist}
-    return render(request, 'cmdb/hlist.html', context)
-
-def hostinfo(request, host_id):
-    #hosts = Host.objects.all()
-    #hostips = Host.objects.first().ip_set.all()
-    hosts = Host.objects.get(id=host_id)
-    hostinfos = hosts.hostinfo_set.all()
-    hostips = hosts.ip_set.all()
-    context = {'hosts': hosts,'hostips': hostips,'hostinfos':hostinfos}
-    return render(request, 'cmdb/hostinfo.html', context)
+    if request.method != 'POST':
+        form = HostForm(instance=host)
+    else:
+        form = HostForm(instance=host, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(
+                reverse('cmdb:hostlist', args=[host.id]))
+    context = {'host': host, 'hostname': hostname,'num': num, 'application': application, 'form': form}
+    return render(request, 'cmdb/edit_host.html', context)
 
 
+def edit_hostinfo(request, host_id):
+    host = HostInfo.objects.get(id=host_id)
+    hostname = host.host
+    manufacturer = host.manufacturer
+    productmode = host.productmode
+    serialnumber = host.serialnumber
+    cpu = host.cpu
+    mem = host.mem
+    disk = host.disk
+
+
+    if request.method != 'POST':
+        form = HostInfoFrom(instance=host)
+    else:
+        form = HostInfoFrom(instance=host, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(
+                reverse('cmdb:hostinfo', args=[host.id]))
+    context = {'hostname': hostname, 'manufacturer': manufacturer,'productmode': productmode,
+     'productmode': productmode,'serialnumber': serialnumber,'cpu': cpu,
+     'mem': mem,'disk': disk, 'form': form}
+    return render(request, 'cmdb/edit_hostinfo.html', context)
