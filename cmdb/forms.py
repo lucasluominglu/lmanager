c678685f53f0120eadaf116abc0dd0b523597216
zbing3 <zbing3@gmail.com>
2017-11-29 22:01:10 +0800

    update upload file

--- cmdb/forms.py
+++ cmdb/forms.py
@@ -10,25 +10,27 @@ class IdcForm(forms.ModelForm):
 
 
 class HostForm(forms.ModelForm):
-	class Meta:
-		model = Host
-		fields = ['idc','hostname','num','application']
-		labels = {'idc': '机房', 'hostname': '主机名','num': '编号', 'application': '应用'}
+    class Meta:
+        model = Host
+        fields = ['idc', 'hostname', 'num', 'application']
+        labels = {'idc': '机房', 'hostname': '主机名',
+                  'num': '编号', 'application': '应用'}
 
 
 class HostInfoForm(forms.ModelForm):
-	class Meta:
-		model = HostInfo
-		fields = ['manufacturer','productmode','serialnumber','cpu','mem','os','disk']
-		labels = {'manufacturer':'厂商','productmode':'产品型号',
-		 'serialnumber':'产品序列号', 'cpu':'CPU核数', 'mem':'内存', 'os':'操作系统', 'disk':'硬盘大小' }
+    class Meta:
+        model = HostInfo
+        fields = ['manufacturer', 'productmode',
+                  'serialnumber', 'cpu', 'mem', 'os', 'disk']
+        labels = {'manufacturer': '厂商', 'productmode': '产品型号',
+                  'serialnumber': '产品序列号', 'cpu': 'CPU核数', 'mem': '内存', 'os': '操作系统', 'disk': '硬盘大小'}
 
 
 class IpForm(forms.ModelForm):
-	class Meta:
-		model = Ip
-		fields = ['ip1','ip2']
-		labels = {'ip1':'ip1','ip2':'ip2'}
+    class Meta:
+        model = Ip
+        fields = ['ip1', 'ip2']
+        labels = {'ip1': 'ip1', 'ip2': 'ip2'}
 
 
 class UploadFileForm(forms.Form):
--- cmdb/views.py
+++ cmdb/views.py
@@ -11,6 +11,7 @@ from django.http import JsonResponse
 from .models import *
 from .forms import *
 
+
 def index(request):
     hosts = Host.objects.all()
     context = {'hosts': hosts}
@@ -22,6 +23,7 @@ def idc(request):
     idc = Idc.objects.all()
     context = {'idc': idc}
     return render(request, 'cmdb/idc.html', context)
+
 
 def new_idc(request):
     if request.method != 'POST':
@@ -52,18 +54,15 @@ def edit_idc(request, idc_id):
     return render(request, 'cmdb/edit_idc.html', context)
 
 
-
 def hostlist(request, idc_id):
     #hostips = Host.objects.first().ip_set.all()
     hosts = Idc.objects.get(id=idc_id)
     hostlists = hosts.host_set.all()
-    context = {'hostlists':hostlists,'hosts': hosts}
+    context = {'hostlists': hostlists, 'hosts': hosts}
     return render(request, 'cmdb/hostlist.html', context)
 
 
-
-
-def  add_host(request,idc_id):
+def add_host(request, idc_id):
     idc = Idc.objects.get(id=idc_id)
 
     if request.method != 'POST':
@@ -75,16 +74,17 @@ def  add_host(request,idc_id):
         hostinfoform = HostInfoForm(request.POST)
         ipform = IpForm(request.POST)
         if hostform.is_valid() and hostinfoform.is_valid() and ipform.is_valid():
-           add_hostform = hostform.save(commit=False)
-           add_hostform.save()
-           add_hostinfoform = hostinfoform.save(commit=False)
-           add_hostinfoform.host = add_hostform
-           add_hostinfoform.save()
-           add_ipform = ipform.save(commit=False)
-           add_ipform.host = add_hostform
-           add_ipform.save()
-           return HttpResponseRedirect(reverse('cmdb:hostlist', args=[idc.id]))
-    context = {'idc': idc, 'hostform':hostform,'hostinfoform':hostinfoform,'ipform':ipform}
+            add_hostform = hostform.save(commit=False)
+            add_hostform.save()
+            add_hostinfoform = hostinfoform.save(commit=False)
+            add_hostinfoform.host = add_hostform
+            add_hostinfoform.save()
+            add_ipform = ipform.save(commit=False)
+            add_ipform.host = add_hostform
+            add_ipform.save()
+            return HttpResponseRedirect(reverse('cmdb:hostlist', args=[idc.id]))
+    context = {'idc': idc, 'hostform': hostform,
+               'hostinfoform': hostinfoform, 'ipform': ipform}
     return render(request, 'cmdb/add_host.html', context)
 
 
@@ -95,7 +95,7 @@ def edit_host(request, host_id):
     ip = Ip.objects.get(host=hosts)
     if request.method != 'POST':
         hostsfrom = HostForm(instance=hosts)
-        idcfrom   = IdcForm(instance=idc)
+        idcfrom = IdcForm(instance=idc)
         hostinfofrom = HostInfoForm(instance=hostinfo)
         ipfrom = IpForm(instance=ip)
     else:
@@ -110,20 +110,34 @@ def edit_host(request, host_id):
             ipfrom.save()
             return HttpResponseRedirect(
                 reverse('cmdb:hostlist', args=[hosts.id]))
-    context = {'hosts': hosts, 'hostsfrom': hostsfrom, 'idcfrom': idcfrom, 'hostinfofrom': hostinfofrom, 'ipfrom': ipfrom}
+    context = {'hosts': hosts, 'hostsfrom': hostsfrom,
+               'idcfrom': idcfrom, 'hostinfofrom': hostinfofrom, 'ipfrom': ipfrom}
     return render(request, 'cmdb/edit_host.html', context)
+
+
+def handle_uploaded_file(f):
+    filename = settings.BASE_DIR + "/name.txt"
+    print(filename)
+    with open(filename, 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+
 
 def upload_file(request):
     if request.method == 'POST':
         #form = UploadFileForm(request.POST, request.FILES)
         form = UploadFileForm(request.POST, request.FILES)
+        # if form.is_valid():
+        # handle_uploaded_file(request.FILES['file'])
+        # form.save()
         if form.is_valid():
-            #handle_uploaded_file(request.FILES['file'])
-            form.save()
+            print("valid")
+            handle_uploaded_file(request.FILES['file'])
             return HttpResponseRedirect(reverse('cmdb:index'))
     else:
         form = UploadFileForm()
     return render(request, 'cmdb/upload_file.html', {'form': form})
+
 
 def pack():
     """打包"""
@@ -132,9 +146,10 @@ def pack():
     local(
         'tar -czvf {0}{1}.tar.gz {2}'.format(app_name, tag, settings.BASE_DIR))
 
+
 def upload():
     env.user = 'root'
-    env.hosts = ['114.67.228.225']
+    env.hosts = ['120.78.186.44']
     env.key_filename = "~/.ssh/id_rsa.pub"
     remote_tmp_dir = '/tmp'
     app_path, app_name = os.path.split(settings.BASE_DIR)
@@ -142,6 +157,7 @@ def upload():
     deploy_file = '{0}{1}.tar.gz'.format(app_name, tag)
     put(deploy_file, remote_tmp_dir)
     run('ls /tmp')
+
 
 def dep():
     env.user = 'root'
@@ -153,51 +169,54 @@ def dep():
     deploy_file = '{0}{1}.tar.gz'.format(app_name, tag)
     put(deploy_file, remote_tmp_dir)
     run('ls /tmp')
-    run('tar -xf {0}/{1} -C /home'.format(remote_tmp_dir,deploy_file))
+    run('tar -xf {0}/{1} -C /home'.format(remote_tmp_dir, deploy_file))
     run('ls /home')
-
 
 
 def get_rollback_file():
     env.user = 'root'
-    env.hosts = ['114.67.228.225']
+    env.hosts = ['120.78.186.44']
     env.key_filename = "~/.ssh/id_rsa.pub"
     app_path, app_name = os.path.split(settings.BASE_DIR)
-    rb_file = run('ls /tmp/{0}*'.format(app_name))
+    # rb_file = run('ls /tmp/{0}*'.format(app_name))
+    rb_file = run('ls /tmp/')
     rb_list = rb_file.split("  ")
     rb_list = [os.path.basename(i) for i in rb_file.split("  ")]
     return rb_list
 
+
 def deploy(request):
-   if request.method == "GET":
+    if request.method == "GET":
         rb_list = execute(get_rollback_file)
         print(rb_list)
         context = {'rb_list': rb_list}
         return render(request, 'cmdb/deploy.html', context)
-    #lif request.method == "POST":
+    # lif request.method == "POST":
     #    pack()
      #   context = {"status": 200, "message": "ok"}
       #  return JsonResponse(context)
 
+
 def packs(request):
     if request.method == "POST":
-        myFile =request.FILES.get("myfile", None)
+        # myFile =request.POST.get("myfile", None)
         execute(pack)
         context = {"status": 200, "message": "ok"}
         return JsonResponse(context)
 
-def  uploads(request):
+
+def uploads(request):
     if request.method == "POST":
         execute(upload)
-        context = {"status":200, "message": "ok"}
+        context = {"status": 200, "message": "ok"}
         return JsonResponse(context)
 
-def  deploys(request):
+
+def deploys(request):
     if request.method == "POST":
         execute(dep)
-        context = {"status":200, "message": "ok"}
+        context = {"status": 200, "message": "ok"}
         return JsonResponse(context)
-
 
 
 def rollback(request):
@@ -205,8 +224,3 @@ def rollback(request):
         name = request.POST.get('name')
         context = {"status": 200, "message": "ok", "name": name}
         return JsonResponse(context)
-
-
-
-
-
